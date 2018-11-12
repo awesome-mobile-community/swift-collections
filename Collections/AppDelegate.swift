bc0fbe28ff56822bc19714c51688c6521a301eb5
christopher.walker <christopher.walker@digital.hmrc.gov.uk>
2018-11-12 20:44:41 +0000

    Initial Commit

---
+++ Collections/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Collections
+//
+//  Created by Chris J W Walker on 12/11/2018.
+//  Copyright © 2018 Chris J W Walker. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
